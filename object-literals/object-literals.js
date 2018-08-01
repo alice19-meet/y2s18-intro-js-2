9ed7f70913d2fbb56f9e188ef92736d2c0d13846
alice19-meet <alice19@meet.mit.edu>
2018-08-01 14:15:39 +0300

    js2

--- object-literals/object-literals.js
+++ object-literals/object-literals.js
@@ -1,1 +1,11 @@
 // Create your 'me' object literal here!
+var me={
+
+    firstName:"Alice",
+    lastName:"Qassabian",
+    favoriteFoods:["grapeleaves", "pizza", "french fries"],
+    age:16,
+    picture:"unicorn.jpeg",
+
+}
+document.write(JSON.stringify(me))
