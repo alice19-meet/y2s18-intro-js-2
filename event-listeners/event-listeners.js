2bc9f0a8d824c6f7743a41a2e30fa2363b63053b
alice19-meet <alice19@meet.mit.edu>
2018-08-02 16:45:10 +0300

    event

--- event-listeners/event-listeners.js
+++ event-listeners/event-listeners.js
@@ -1,5 +1,5 @@
 // YOUR TASK: Add more pictures!
-var pictures = ['./imgs/dog.jpg'];
+var pictures = ['./imgs/dog.jpg','1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 var currentIndex = 0;
 
 function showNextPicture() {
@@ -7,7 +7,16 @@ function showNextPicture() {
   // if currentIndex is too large, start from the beginning again
   if (currentIndex >= pictures.length) {
     currentIndex = 0;
+
   }
 
   // YOUR TASK: Finish this function!
+    // event.target is the object that was clicked
+	document.getElementsByTagName("img")[0].src = pictures[currentIndex];
 }
+
+
+
+  document.getElementsByTagName("img")[0].addEventListener("click", function (event) {
+  	showNextPicture();
+  });
--- html-events/html-events.js
+++ html-events/html-events.js
@@ -1,1 +1,7 @@
 // Put all your JavaScript in this file!
+function box(){
+	var tea=prompt("pick a color");
+	document.getElementsByTagName("body");
+	var v=document.getElementsByTagName("body");
+	v[0].style.backgroundColor=tea
+}
